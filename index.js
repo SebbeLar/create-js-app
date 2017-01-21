6bdb01fb706e2ab2ac80abd211b64a502c6cf4c7
Sebastian Larsson <sebberlarsson@hotmail.com>
2017-01-21 20:10:45 +0100

    Started with the CLI inteface

---
+++ index.js
@@ -0,0 +1,60 @@
+const chalk       = require('chalk');
+const clear       = require('clear');
+const CLI         = require('clui');
+const figlet      = require('figlet');
+const inquirer    = require('inquirer');
+const Preferences = require('preferences');
+const Spinner     = CLI.Spinner;
+const GitHubApi   = require('github');
+const _           = require('lodash');
+const git         = require('simple-git')();
+const touch       = require('touch');
+const fs          = require('fs');
+const files       = require('./lib/files');
+
+clear();
+console.log(
+  chalk.yellow(
+    figlet.textSync('create-js-app', { horizontalLayout: 'full' })
+  )
+);
+
+//if (files.directoryExists('.git')) {
+  //console.log(chalk.red('This project is already setup'));
+  //process.exit();
+//}
+
+function getGithubCredentials(callback) {
+  const questions = [
+    {
+      name: 'username',
+      type: 'input',
+      message: 'Enter your Github username or e-mail address:',
+      validate: function(value) {
+        if (value.length) {
+          return true;
+        } else {
+          return 'Please enter your username or e-mail address';
+        }
+      }
+    },
+    {
+      name: 'password',
+      type: 'password',
+      message: 'Enter your password:',
+      validate: function(value) {
+        if (value.length) {
+          return true;
+        } else {
+          return 'Please enter your password:';
+        }
+      }
+    }
+  ];
+
+  inquirer.prompt(questions).then(callback);
+}
+
+getGithubCredentials(function() {
+  console.log(arguments);
+});
